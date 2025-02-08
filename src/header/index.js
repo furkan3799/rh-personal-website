e2403039d43785c98945aed088e46bef342756cd
furkan-asani <asfu1011@h-ka.de>
2025-02-08 11:41:03 +0100

    Remove the theme toggle

--- src/header/index.js
+++ src/header/index.js
@@ -3,7 +3,6 @@ import "./style.css";
 import { VscGrabber, VscClose } from "react-icons/vsc";
 import { Link } from "react-router-dom";
 import { logotext, socialprofils } from "../content_option";
-import Themetoggle from "../components/themetoggle";
 
 const Headermain = () => {
   const [isActive, setActive] = useState("false");
@@ -21,7 +20,6 @@ const Headermain = () => {
             {logotext}
           </Link>
           <div className="d-flex align-items-center">
-            <Themetoggle />
             <button className="menu__button  nav_ac" onClick={handleToggle}>
               {!isActive ? <VscClose /> : <VscGrabber />}
             </button>
