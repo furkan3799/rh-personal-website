de0fd3220c435f6235415c40691d1d553549b8a3
furkan-asani <asfu1011@h-ka.de>
2025-02-09 13:03:18 +0100

    Fix authentication check in ProtectedRoute and AuthService

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -3,7 +3,7 @@ import { Navigate } from "react-router-dom";
 import authService from "../../services/auth.service";
 
 export const ProtectedRoute = ({ children }) => {
-  const isAuthenticated = true || authService.isAuthenticated();
+  const isAuthenticated = authService.isAuthenticated();
 
   if (!isAuthenticated) {
     // Redirect to login if not authenticated
