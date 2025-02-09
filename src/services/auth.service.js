484427115da875fd4c4353213e168d589096d0fc
furkan-asani <asfu1011@h-ka.de>
2025-02-09 11:19:24 +0100

    Fix API URL configuration order in auth service

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { jwtDecode } from "jwt-decode";
 
-const API_URL = "http://localhost:3000/api" || process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3000/api";
 
 class AuthService {
   async login(username, password) {
